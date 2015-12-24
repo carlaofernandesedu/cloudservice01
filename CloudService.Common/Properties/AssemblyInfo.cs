3cc238ffe0d5e07e15a8cb64d71c4bda61e3951a
Carlos Melo <carlos.melo_desenv@hotmail.com>
2015-12-24 15:30:09 -0200

    criando o arquivo nuget

--- CloudService.Common/Properties/AssemblyInfo.cs
+++ CloudService.Common/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("CloudService.Common")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Base para implementacao de Log")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Author")]
 [assembly: AssemblyProduct("CloudService.Common")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
