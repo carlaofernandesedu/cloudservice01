5b3e720055ce207581b97d17e23311cc03e867c0
Carlos Melo <carlaofernandesedu@gmail.com>
2016-01-08 10:03:51 -0200

    Montagem do Projeto de POC simples

--- WebLogPoc/default.aspx
+++ WebLogPoc/default.aspx
@@ -11,6 +11,7 @@
     <div>
         <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
         <asp:Button ID="Button1" runat="server" Text="Button" OnClick="Button1_Click" />   
+        <asp:Label ID="Label1" runat="server"></asp:Label>
     </div>
     </form>
 </body>
--- WebLogPoc/default.aspx.cs
+++ WebLogPoc/default.aspx.cs
@@ -18,9 +18,13 @@ namespace WebLogPoc
 
         protected void Button1_Click(object sender, EventArgs e)
         {
-            //Verficar o Web.Config que configura o procedimento para será Gravado e que formato será gravado
+            //Verficar o Web.Config que configura o procedimento para qual local será Gravado e que formato será gravado
+            //http://www.eidias.com/blog/2012/10/15/simple-logging-with-use-of-systemdiagnostictrace
+            //https://msdn.microsoft.com/en-us/library/hfaf9h0e(v=vs.110).aspx
             ILogger objlog = CloudService.Common.Logging.LoggerFactory<FactoryLogger>.GetLogger();
             objlog.Information("campo {0} informado", TextBox1.Text);
+            Label1.Text = "Procedimento de Log realizado as" + DateTime.Now.ToLongTimeString();
+            
             
         }
     }
